c273aa2c0c3678c61aa3dc6e8341dc512a2bb8e4
Praneeth Kulkarni <praneeth.u.k@gmail.com>
2025-09-15 20:53:16 +0530

    ip for Render

---
+++ backend/ip.js
@@ -0,0 +1,6 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+  res.send({ ip: req.ip || req.socket.remoteAddress || req.connection.remoteAddress });
+});
+app.listen(3000, () => console.log('Server running on port 3000'));
